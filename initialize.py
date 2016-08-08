29e2390cfcf5490d00e1903fbe1e438b2db240a1
shiran <shiran@meet.mit.edu>
2016-08-08 10:33:47 +0300

    hi

--- initialize.py
+++ initialize.py
@@ -17,6 +17,7 @@ Tehila.fullname="Tehila Pelled"
 Tehila.email="tehilapelled16@gmail.com"
 Tehila.password="password"
 Tehila.about_myself = "Hi my name is Tehila and I like scuba diving, programming and i am 16 years old."
+Tehila.photo = "https://www.colourbox.com/preview/8103563-cute-girl-cartoon.jpg"
 
 			 
 
@@ -26,9 +27,9 @@ Tehila.about_myself = "Hi my name is Tehila and I like scuba diving, programming
 Amos = User(fullname="Amos Ro ", 
 			  email="amosamos@gmail.com", 
 			  password="password",
-  			  about_myself = "Hi I am Amos"
-
-			  )
+  			  about_myself = "Hi I am Amos",
+			  photo = "https://www.colourbox.com/preview/8103563-cute-girl-cartoon.jpg" 
+	)
 Amos2 = User(fullname="Amos Ro ", 
 			  email="amosamos@gmail.com", 
 			  password="password",
@@ -37,7 +38,7 @@ Amos2 = User(fullname="Amos Ro ",
 
 simple_q=['Whats your age?','Gender(male/female/other)?','Do you have brothers or sisters?How many?','Where do you learn? ', 'What is your nationality','Where do you live? ', ' Favourite color? ' ]
 deep_q=['Favourite movie? ', 'Favourite food?', 'Do you have any pets? ','X-box or PlayStation? ', 'Favourite subject in school? ','What is your hobies? ', 'Are you religious or spirituals?','Do you consider yourself an introvert or an extrovert? ', 'Are you open minded or not? ', 'Is what you are doing now what you always wanted to do growing up? ', 'Describe you are relashionship with your parents in one word? ', 'Describe the meaning of life in one word? ','What kind of parent do you think you will be? ','Are you a leader or a follower ','What type of friend are you? ','on scale of 1-10 how onest are you? ', 'What are 2 things that you cannot live without? ','Do you prefer giving or receiving ', 'Describe yourself in two words? ']
-'''
+
 for i in simple_q:
     a=Questions(
         text=i,
@@ -56,7 +57,6 @@ for i in deep_q:
 
 session.add(Tehila)
 session.add(Amos)
-'''
 #Amos.my_friends.append(Tehila)
 
 session.commit()
